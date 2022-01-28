8030685b25d96316a6b93a2fa9ad626ce3129b49
billzzhang <billzhang345@gmail.com>
2022-01-27 22:50:29 -0500

    Package

---
+++ src/elevator/Floor.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package elevator;
+
+/**
+ * @author 
+ *
+ */
+public class Floor {
+	ArrayList<Elevator> elevators;
+	
+}
---
+++ src/elevator/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author Bill
+ *
+ */
+package elevator;
