27e14f80cbb7f1e67b89727c60e4933eef5ab108
j769chen <j769chen@uwaterloo.ca>
2022-02-01 00:02:32 -0500

    added javadocs and constructor to scheduler

--- src/main/scheduler/Scheduler.java
+++ src/main/scheduler/Scheduler.java
@@ -4,22 +4,50 @@ import main.common.DirectionalLamp;
 import main.elevator.ElevatorButton;
 import main.floor.FloorButton;
 
+/**
+ * Class to represent the backend server to handle communication between elevator components
+ */
 public class Scheduler {
+	
 	/**
+	 * Default constructor
+	 */
+	public Scheduler() {
+		
+	}
+	
+	
+	/**
+	 * Function to send messages to floor
 	 * 
 	 */
 	void notifyFloor() {
 		
 	}
 	
+	
+	/**
+	 * Function to send messages to elevator
+	 * 
+	 */
 	void notifyElevator() {
 		
 	}
 	
+	/**
+	 * Function that returns the value of an elevator button press (which floor has been requested)
+	 * 
+	 * @return an integer representing the requested floor
+	 */
 	private int getElevatorButtonPress(ElevatorButton elevatorButton) {
 		return elevatorButton.getButtonPressed();
 	}
 	
+	/**
+	 * Function that returns the value of a floor button press (elevator going up or down)
+	 * 
+	 * @return an enum value (UP/DOWN)
+	 */
 	private DirectionalLamp getFloorButtonPress(FloorButton floorButton) {
 		return floorButton.getDirectionalLamp();
 	}
