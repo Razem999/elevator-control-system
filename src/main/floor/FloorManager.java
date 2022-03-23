1ef080a3bc316d2b4ba5eeea56042ff0aa116a44
James Chen <j769chen@uwaterloo.ca>
2022-03-22 21:10:53 -0400

    Refactored to only use floor manager (#35)
    * refactored so responsibility to send all lines from file is in floor manager
    
    * remove inputManager

--- src/main/floor/FloorManager.java
+++ src/main/floor/FloorManager.java
@@ -6,12 +6,15 @@ package main.floor;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Scanner;
 import static java.time.temporal.ChronoUnit.MILLIS;
 
 import main.common.Logger;
 import main.common.PacketHandler;
+import main.common.Input.Fault;
+import main.common.Input.Input;
 import main.common.Input.Instructions;
 import main.common.ByteConverter;
 import main.common.Constants;
@@ -36,13 +39,16 @@ public class FloorManager {
 	private int timeWithoutRequests = 0;
 	/** PacketHandler for dealing with UDP communication */
 	private PacketHandler packetHandler;
+	/** list of inputs to process */
+	private final ArrayList<Input> inputList;
 
 	/**
 	 * Constructor that accepts a scheduler and floor number
 	 * @param scheduler
 	 * @param floorNumber
 	 */
-	public FloorManager(int numFloors) {
+	public FloorManager(int numFloors, String filename) {
+		inputList = new ArrayList<>();
 		this.button = new FloorButton();
 		this.instructions = new ArrayList<>();
 		this.packetHandler = new PacketHandler(Constants.SCHEDULER_PORT, Constants.FLOOR_MANAGER_PORT);
@@ -55,9 +61,80 @@ public class FloorManager {
 
 		this.floorButtons = new FloorButton[numFloors];
 		this.floorLamps = new FloorLamp[numFloors];
-
+		
 		this.logger = new Logger("FLOOR");
 		logger.log("Starting...");
+		
+		getInput(filename);
+	}
+	
+	/**
+     * Verifies that the input line is valid and returns true if it is, false if it is not 
+     * (i.e. current floor = destination, current floor != this floor, direction is wrong) 
+     * 
+     * @returns boolean
+     */
+    private boolean verifyInstructions(String[] commands) {
+    	if (commands.length < 4) {
+    		return false;
+    	}
+		// if the current and destination floors are the same
+		if (commands[1].equals(commands[3])) {
+			return false;
+		}
+
+		// make sure floors in the instruction are within legal bounds
+		if (Integer.valueOf(commands[1]) < 1 || Integer.valueOf(commands[1]) > Constants.NUM_FLOORS) {
+			return false;
+		}
+
+		if (Integer.valueOf(commands[3]) < 1 || Integer.valueOf(commands[3]) > Constants.NUM_FLOORS) {
+			return false;
+		}
+
+		// if the previous checks passed, we just need to verify that the direction is correct
+		if (Integer.valueOf(commands[1]) < Integer.valueOf(commands[3])) {
+			return commands[2].equals("Up");
+		}
+
+		return commands[2].equals("Down");
+	}
+    
+    /**
+     * 
+     * @param commands
+     * @return
+     */
+    private boolean verifyFault(String[] commands) {
+    	// TODO TODO TODO
+    	return commands[1].equals("Type");
+    }
+    
+    /**
+	 * Reads in a text input file to populate the instructions
+	 * @param file - string file name of input
+	 */
+	private void getInput(String file) {
+		logger.log("Reading input file...");
+		File input = new File(file);
+		try {
+			Scanner inputReader = new Scanner(input);
+			while (inputReader.hasNextLine()) {
+			  String line = inputReader.nextLine();
+			  String[] commands = line.split(" ");
+
+			  // check if they are valid instructions or faults
+			  if (verifyInstructions(commands)) {
+				  inputList.add(new Instructions(commands));
+			  } else if (verifyFault(commands)) {
+				  inputList.add(new Fault(commands));
+			  }
+			}
+			logger.log("Added all inputs to input list");
+			inputReader.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	/**
@@ -90,22 +167,48 @@ public class FloorManager {
     	logger.log("Response: " + new String(returned, StandardCharsets.UTF_8));
     	return returned[0] == (byte) 0;
     }
-
+    
+	/**
+	 * Sends an input to its corresponding destination based on its class.
+	 * 
+	 * @param input Input - input to send. May be Instructions or Fault.
+	 */
+	private void sendInput(Input input) {
+		if (input instanceof Instructions) {
+			sendInstruction((Instructions) input);
+			logger.log("Sent instructions to floor manager");
+		} else {
+			Fault fault = (Fault) input;
+			int targetPort = Constants.ELEVATOR_STARTING_PORT_NUMBER + fault.getElevatorId(); // uncomment other line when we change elevator ports
+//			int targetPort = Constants.ELEVATOR_STARTING_PORT_NUMBER + fault.getElevatorId() * 10;
+			PacketHandler elevatorHandler = new PacketHandler(targetPort);
+			elevatorHandler.send(new byte[] { -1 }); // TODO: actual byte format for faults
+			logger.log("Sent fault to elevator");
+		}
+	}
+	
     /**
      * Receives input from the input manager and forwards it to the Scheduler.
      */
     private void simulate() {
-    	// TODO: need a better way of doing this
-    	while (instructions.size() < 8) {
-    		receiveInstruction();
-    	}
-    	while (!instructions.isEmpty()) {
-    		sendInstruction(instructions.remove(0));
-    	}
+    	LocalTime prevTime = null;
+		for (Input input : inputList) {
+			sendInput(input);
+			if (prevTime != null) {
+				try {
+					// Sleep for the difference in time between each request
+				    Thread.sleep(Math.abs(MILLIS.between(input.getTime(), prevTime)));
+				} 
+				catch(InterruptedException ex) {
+				    Thread.currentThread().interrupt();
+				}
+			}
+			prevTime = input.getTime();
+		}
     }
 
 	public static void main(String[] args) {
-		FloorManager floor = new FloorManager(Constants.NUM_FLOORS);
+		FloorManager floor = new FloorManager(Constants.NUM_FLOORS, "mockInput.txt");
 		floor.simulate();
 	}
 }
