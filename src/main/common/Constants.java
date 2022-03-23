f1a17afe829b737973cf9bbde9c73cc44d63ff90
Razem Muhammad Shahin <razemshahin@cmail.carleton.ca>
2022-03-23 11:37:28 -0400

    Update DOOR_OPEN to DOOR_OPEN_TIME

--- src/main/common/Constants.java
+++ src/main/common/Constants.java
@@ -16,7 +16,7 @@ public class Constants {
 	/** This value is used in conjunction with the ELEVATOR_STARTING_PORT_NUMBER to assign each Elevator a unique port number, incremented by 10*/
 	public static final int ELEVATOR_INCREMENT = 10; 
 	/** The amount of time the elevator door stays open for before closing*/
-	public static final int DOOR_OPEN = 4000;
+	public static final int DOOR_OPEN_TIME = 4000;
 	/** This value is used in conjunction with the Elevator number to assign each Elevator Door a unique port number */
 	public static final int ELEVATOR_DOOR_PORT_NUMBER = 1;
 	/** The amount of time it takes for an Elevator to move floors */
